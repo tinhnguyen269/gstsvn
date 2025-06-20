134cbc453c025980e60a9289ba11e4c09b53cd3b
tinh.nguyenvan <tinh.nguyenvan@vti.com.vn>
2025-06-20 09:41:02 +0700

    update template customer

---
+++ src/main/java/com/example/serviceapp/customer/post/repositoty/PostRepository.java
@@ -0,0 +1,4 @@
+package com.example.serviceapp.customer.post.repositoty;
+
+public interface PostRepository {
+}
---
+++ src/main/java/com/example/serviceapp/customer/post/repositoty/impl/PostRepositoryImpl.java
@@ -0,0 +1,7 @@
+package com.example.serviceapp.customer.post.repositoty.impl;
+
+import org.springframework.stereotype.Repository;
+import com.example.serviceapp.customer.post.repositoty.PostRepository;
+@Repository
+public class PostRepositoryImpl implements PostRepository {
+}
