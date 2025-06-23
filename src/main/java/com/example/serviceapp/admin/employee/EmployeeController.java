50b4ad1be87106b8264bfb03467be5668dbbe02d
tinhnguyen269 <tinhnguyen180507@gmail.com>
2025-06-24 00:35:14 +0700

    update template admin

---
+++ src/main/java/com/example/serviceapp/admin/employee/EmployeeController.java
@@ -0,0 +1,14 @@
+package com.example.serviceapp.admin.employee;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class EmployeeController {
+    @GetMapping("/employee")
+    public String doInit() {
+        return "admin/employee";
+    }
+}
