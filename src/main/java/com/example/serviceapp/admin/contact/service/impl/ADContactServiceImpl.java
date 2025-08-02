07477bf8414e967045db0daba39dc0c3441883d6
tinhnguyen269 <tinhnguyen180507@gmail.com>
2025-08-02 22:16:51 +0700

    fix bug ui

--- src/main/java/com/example/serviceapp/admin/contact/service/impl/ADContactServiceImpl.java
+++ src/main/java/com/example/serviceapp/admin/contact/service/impl/ADContactServiceImpl.java
@@ -2,18 +2,24 @@ package com.example.serviceapp.admin.contact.service.impl;
 
 import com.example.serviceapp.admin.contact.repository.ADContactRepository;
 import com.example.serviceapp.admin.contact.service.ADContactService;
+import com.example.serviceapp.admin.service.repository.ADServiceRepository;
 import com.example.serviceapp.common.entity.Customer;
+import com.example.serviceapp.common.entity.Services;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ADContactServiceImpl implements ADContactService {
 
     public final ADContactRepository contactRepository;
+    public final ADServiceRepository serviceRepository;
 
-    public ADContactServiceImpl(ADContactRepository contactRepository) {
+    public ADContactServiceImpl(ADContactRepository contactRepository, ADServiceRepository serviceRepository) {
         this.contactRepository = contactRepository;
+        this.serviceRepository = serviceRepository;
     }
 
     @Override
@@ -46,6 +52,11 @@ public class ADContactServiceImpl implements ADContactService {
     public void delete(Customer customer) {
         customer.setDeleteFlag(1);
         contactRepository.save(customer);
+    }
+
+    @Override
+    public List<Services> getAllServices() {
+        return serviceRepository.getAllService();
     }
 
 }
--- src/main/java/com/example/serviceapp/admin/password/PasswordController.java
+++ src/main/java/com/example/serviceapp/admin/password/PasswordController.java
@@ -2,10 +2,12 @@ package com.example.serviceapp.admin.password;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/admin/password")
 public class PasswordController {
-    @GetMapping("/password")
+    @GetMapping("")
     public String showPasswordPage() {
         return "admin/authenticate/password";
     }
