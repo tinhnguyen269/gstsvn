c27ec464860b9043009e988b2e483868761556ee
tinhnguyen269 <tinhnguyen180507@gmail.com>
2025-10-23 07:36:13 +0700

    fix image static

--- src/main/java/com/example/serviceapp/ServiceAppApplication.java
+++ src/main/java/com/example/serviceapp/ServiceAppApplication.java
@@ -2,11 +2,23 @@ package com.example.serviceapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class ServiceAppApplication {
 
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("MYSQL_URL", dotenv.get("MYSQL_URL"));
+        System.setProperty("MYSQL_USER", dotenv.get("MYSQL_USER"));
+        System.setProperty("MYSQL_PASSWORD", dotenv.get("MYSQL_PASSWORD"));
+        System.setProperty("CLOUDINARY_CLOUD_NAME", dotenv.get("CLOUDINARY_CLOUD_NAME"));
+        System.setProperty("CLOUDINARY_API_KEY", dotenv.get("CLOUDINARY_API_KEY"));
+        System.setProperty("CLOUDINARY_API_SECRET", dotenv.get("CLOUDINARY_API_SECRET"));
+        System.setProperty("SPRING_MAIL_USERNAME", dotenv.get("SPRING_MAIL_USERNAME"));
+        System.setProperty("SPRING_MAIL_PASSWORD", dotenv.get("SPRING_MAIL_PASSWORD"));
+        System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
         SpringApplication.run(ServiceAppApplication.class, args);
     }
 }
