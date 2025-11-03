280efacec09d5a502f470d7d4ac1501a60c47b6f
tinhnguyen269 <tinhnguyen180507@gmail.com>
2025-11-03 22:55:40 +0700

    fix duplication phone

--- src/main/java/com/example/serviceapp/admin/contact/repository/ADContactRepository.java
+++ src/main/java/com/example/serviceapp/admin/contact/repository/ADContactRepository.java
@@ -21,10 +21,6 @@ public interface ADContactRepository extends JpaRepository<Customer, Long> {
     Page<Customer> searchCustomer(@Param("keyword") String keyword, Pageable pageable);
 
     @Query("SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END " +
-           "FROM Customer c WHERE c.phoneNumber = :phoneNumber AND c.deleteFlag = 0")
-    boolean isPhoneNumberExists(String phoneNumber);
-
-    @Query("SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END " +
            "FROM Customer c WHERE c.phoneNumber = :phoneNumber AND c.deleteFlag = 0 AND c.customerId <> :customerId")
     boolean isPhoneNumberUpdateExists(String phoneNumber, Long customerId);
 
